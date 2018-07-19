4ff219391e442b67cd4991dfc0c10c6bfcbe31dc
River-Prince <stocki9000@gmail.com>
2018-07-19 21:03:01 +0100

    Added initial code - Untested

---
+++ src/ergo/HttpProxy.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ergo
+{
+	public class HttpProxy : TcpProxy
+	{
+		private const int CONNECTION_ENDED_WAIT = 30000;
+
+		protected override void ProcessClientConnection(TcpClient client)
+		{
+			NetworkStream clientStream = client.GetStream();
+			StreamReader clientReader = new StreamReader(clientStream);
+			StreamWriter clientWriter = new StreamWriter(clientStream);
+
+			TcpClient server = null;
+			NetworkStream serverStream = null;
+			StreamReader serverReader = null;
+			StreamWriter serverWriter = null;
+
+			try
+			{
+				// Get the first line of the HTTP request which will tell us what to do
+				// and where to go. We can use stream reader here because the first request
+				// will always be UTF8.
+				string request = clientReader.ReadLine();
+
+				ParseRequestDetails(request, out string method, out string host, out int port, out string httpVersion);
+
+				ConnectToRemote(
+					host,
+					port,
+					out server,
+					out serverStream,
+					out serverReader,
+					out serverWriter);
+
+				if (method == "CONNECT")
+				{
+					// If its a connect request this doesn't need to be sent to the remote
+					// so just read it off the stream.
+					while (String.IsNullOrEmpty(clientReader.ReadLine()) == false) ;
+
+					SendConnectResponse(clientWriter, httpVersion);
+				}
+				else
+				{
+					// If we aren't using CONNECT then we have already read the first line
+					// that we want to send to the server. We cant just write it alone as the 
+					// server while reject it. We need to send the entire header all at once.
+					serverWriter.WriteLine(request);
+
+					string line;
+					while (String.IsNullOrEmpty(line = clientReader.ReadLine()) == false)
+					{
+						serverWriter.WriteLine(line);
+					}
+
+					serverWriter.WriteLine();
+
+					serverWriter.Flush();
+				}
+
+				// Tunnels streams into each other sync. This is basically a transparent proxy.
+				// When both are done then the connections have been closed.
+
+				Task clientToServerTask = clientStream.CopyToAsync(serverStream);
+				Task serverToClientTask = serverStream.CopyToAsync(clientStream);
+
+				Task.WaitAny(
+					clientToServerTask,
+					serverToClientTask);
+
+				Task.WaitAll(
+					new Task[] { clientToServerTask, serverToClientTask },
+					CONNECTION_ENDED_WAIT);
+			}
+			finally
+			{
+				try
+				{
+					DisposeConnection(client, clientStream, clientReader, clientWriter);
+					DisposeConnection(server, serverStream, serverReader, serverWriter);
+				}
+				catch
+				{
+				}
+			}
+		}
+
+		/// <summary>
+		/// Parses the first line of a HTTP request to get the action and address info
+		/// </summary>
+		private void ParseRequestDetails(
+			string request,
+			out string method,
+			out string host,
+			out int port,
+			out string httpVersion)
+		{
+			string[] requestParts = request.Split(' ');
+
+			method = requestParts[0];
+			httpVersion = requestParts[2];
+
+			Uri uri = new Uri(requestParts[1]);
+
+			// CONNECT doesnt send the scheme, because it isnt needed.
+			// But Uri cant parse URLs without a scheme, so we just add one.
+			if (method == "CONNECT")
+			{
+				uri = new Uri("http://" + requestParts[1]);
+			}
+
+			host = uri.Host;
+			port = uri.Port;
+		}
+
+		private void SendConnectResponse(StreamWriter writer, string httpVersion)
+		{
+			writer.WriteLine(httpVersion + " 200 Connection established");
+			writer.WriteLine();
+			writer.Flush();
+		}
+	}
+}
---
+++ src/ergo/TcpProxy.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ergo
+{
+    public abstract class TcpProxy
+    {
+		private IList<TcpListener> _listeners = new List<TcpListener>();
+
+		public IList<IPEndPoint> ListenerAddresses { get; } = new List<IPEndPoint>();
+
+		public void Start()
+		{
+			if (ListenerAddresses.Count > 0)
+			{
+				_listeners.Clear();
+
+				foreach (IPEndPoint address in ListenerAddresses)
+				{
+					TcpListener listener = new TcpListener(address);
+
+					listener.Start();
+
+					listener.BeginAcceptTcpClient(OnAcceptConnection, listener);
+
+					_listeners.Add(listener);
+				}
+			}
+		}
+
+		public void Stop()
+		{
+			foreach (TcpListener listener in _listeners)
+			{
+				listener.Stop();
+			}
+		}
+
+		private void OnAcceptConnection(IAsyncResult asyncResult)
+		{
+			TcpListener listener = (TcpListener)asyncResult.AsyncState;
+
+			try
+			{
+				TcpClient client = listener.EndAcceptTcpClient(asyncResult);
+
+				Task.Run(() => { ProcessClientConnection(client); });
+
+			}
+			catch (ObjectDisposedException)
+			{
+				return;
+			}
+
+			// Start the call to accept another connection
+			if (listener != null)
+			{
+				listener.BeginAcceptTcpClient(OnAcceptConnection, listener);
+			}
+		}
+
+		protected void ConnectToRemote(
+			string host,
+			int port,
+			out TcpClient server,
+			out NetworkStream stream,
+			out StreamReader reader,
+			out StreamWriter writer)
+		{
+			server = new TcpClient();
+			server.Connect(host, port);
+
+			stream = server.GetStream();
+			reader = new StreamReader(stream);
+			writer = new StreamWriter(stream);
+		}
+
+		protected void DisposeConnection(
+			TcpClient client,
+			NetworkStream stream,
+			StreamReader reader,
+			StreamWriter writer)
+		{
+			if (writer != null)
+			{
+				writer.Close();
+				writer.Dispose();
+			}
+
+			if (reader != null)
+			{
+				reader.Close();
+				reader.Dispose();
+			}
+
+			if (stream != null)
+			{
+				stream.Close();
+				stream.Dispose();
+			}
+
+			if (client != null)
+			{
+				client.Close();
+			}
+		}
+
+		protected abstract void ProcessClientConnection(TcpClient client);
+	}
+}
